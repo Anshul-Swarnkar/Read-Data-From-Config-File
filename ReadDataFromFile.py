36297ef0dfe4327ece50445d744a41c857c0bd73
Anshul <anshul.swarnkar1990@gmail.com>
2021-06-20 07:15:26 +0530

    Added browser activity in file

--- ReadDataFromFile.py
+++ ReadDataFromFile.py
@@ -1,9 +1,14 @@
-from configparser import ConfigParser
+#from configparser import ConfigParser
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+driver.get('http://www.google.com')
 
 # create object of configparser file
-config= ConfigParser()
+#config= ConfigParser()
 
 # to read data from config file
-config.read("C:/Users/Dell/PycharmProjects/PythonTutorials/Read Data From Config File/Config.cfg")
+#config.read("C:/Users/Dell/PycharmProjects/PythonTutorials/Read Data From Config File/Config.cfg")
 
 print(config.get("Section1","password"))
